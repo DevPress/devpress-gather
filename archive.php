700501785844d4135146361f77bde9dd2286b829
Devin Price <devin@wptheming.com>
2014-08-13 19:05:20 -0500

    Better gallery styling.

--- archive.php
+++ archive.php
@@ -9,6 +9,7 @@
 
 get_header(); ?>
 
+	<?php if ( is_category() || is_tag() || is_author() || is_day() || is_year() ) : ?>
 	<header class="page-header">
 		<h1 class="page-title">
 		<?php
@@ -30,33 +31,6 @@ get_header(); ?>
 		elseif ( is_year() ) :
 			printf( __( 'Year: %s', 'gather' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'gather' ) ) . '</span>' );
 
-		elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-			_e( 'Asides', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-			_e( 'Galleries', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-			_e( 'Images', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-			_e( 'Videos', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-			_e( 'Quotes', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-			_e( 'Links', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-			_e( 'Statuses', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-			_e( 'Audios', 'gather' );
-
-		elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-			_e( 'Chats', 'gather' );
-
 		else :
 			_e( 'Archives', 'gather' );
 
@@ -64,6 +38,7 @@ get_header(); ?>
 		?>
 		</h1>
 	</header><!-- .page-header -->
+	<?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
