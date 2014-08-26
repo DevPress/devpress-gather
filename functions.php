fd21dad40782e289759aab98863eff0e14c0c346
Devin Price <devin@wptheming.com>
2014-08-26 12:07:39 -0500

    Missing semicolon.

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function gather_setup() {
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
 	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 720, 1200 )
+	set_post_thumbnail_size( 720, 1200 );
 	add_image_size( 'gather-archive', 560, 999 );
 
 	// Registers menu above the site title
